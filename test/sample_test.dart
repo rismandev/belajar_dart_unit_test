c00217d9c5b61f40b89d2a88edefba58112cce02
Risman Abdilah <risman.abdilah@frisidea.com>
2022-11-08 12:27:54 +0700

    Initialization Dart Unit Test

---
+++ test/sample_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+
+void main() {
+  test("Contoh test", () {
+    // Todo
+  });
+
+  test("Contoh test 2", () {
+    // Todo
+  });
+
+  test("Contoh test 3", () {
+    // Todo
+  });
+}
